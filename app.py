6c89e352e2db8c1b31728317619c0dd5edc29354
Mario Henrique <102007914+mhsousa@users.noreply.github.com>
2024-08-09 12:38:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ desig=pd.read_excel('dados_dashboard.xlsx', sheet_name="Desigualdade")
 with tab1:
     ...
 with tab2:
-    fig = px.line(criac_emp, x='Ano', y="Taxa de Criação de Empregos (JC)- %", colors=["#FF0000"])
+    fig = px.line(criac_emp, x='Ano', y="Taxa de Criação de Empregos (JC)- %")
     fig.update_layout(
         title="Taxa de Criação de Empregos (JC)- %")
 
