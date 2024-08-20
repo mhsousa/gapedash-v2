ad0eb0b6a27a58ffb6dccdad9eece3475094106a
Mario Henrique <102007914+mhsousa@users.noreply.github.com>
2024-08-20 13:53:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ with tab2:
    
     st.markdown("""
 	Este é um exemplo de texto formatado em markdown. 
-	**Você pode usar negrito**, *itálico*, e até mesmo `código`.
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend, velit et malesuada euismod, risus massa convallis dui, ac bibendum libero eros non nisi. Aliquam erat volutpat. Mauris eros augue, eleifend suscipit quam non, faucibus lacinia erat. Nulla a mollis dolor. Vivamus ultricies, neque eget hendrerit congue, turpis sem condimentum libero, et pretium ipsum nibh quis ex. Curabitur sollicitudin, nisi ac ornare mollis, nibh mi ullamcorper urna, vel aliquet ipsum felis eu metus. Nam sollicitudin magna vitae est aliquet, feugiat lobortis nulla vehicula. Sed aliquet vulputate ex, et lobortis metus auctor in. Duis consequat mi eu ligula semper, eu fermentum augue luctus. Nullam sed finibus eros. Cras nec aliquam orci. Sed ante libero, tempus vel pretium vitae, rutrum et lacus. Donec vitae urna malesuada odio commodo molestie. Praesent varius elit urna, ac aliquam nulla tempus id.,
 	* **Listas:**
 	  * Item 1
 	  * Item 2
