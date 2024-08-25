b7eff3ac820521b66935999a470f5484f9c4c62e
Mario Henrique <102007914+mhsousa@users.noreply.github.com>
2024-08-25 10:35:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ with tab1:
     legend=dict(x=0, y=1, orientation='h'))
     fig19.update_xaxes(title_text="Mês", showgrid=True, gridwidth=1, gridcolor='LightPink')
     fig19.update_traces(mode='lines+markers', marker=dict(size=7, color='DarkSlateGrey'))
-    add_hline_with_annotation(fig16,y_value=inf_mens["Alimentação e bebidas"].mean(),text=str(round(inf_mens["Alimentação e bebidas"].mean(),3)), line_color="blue",pos=0.15)    
+    add_hline_with_annotation(fig19,y_value=inf_mens["Alimentação e bebidas"].mean(),text=str(round(inf_mens["Alimentação e bebidas"].mean(),3)), line_color="blue",pos=0.15)    
     st.plotly_chart(fig19)
     st.markdown("""
 	<p style='font-family: Arial, sans-serif; font-size: 16px;'>
