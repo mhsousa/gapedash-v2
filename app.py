1a0df327d319c25afa7ac51832ce1f267f6af292
Mario Henrique <102007914+mhsousa@users.noreply.github.com>
2024-08-27 13:06:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,12 +64,12 @@ def add_hline_with_annotation(fig, y_value, text, line_color='red', line_dash='d
 
 with tab1:
     st.markdown("""<p style='font-family: Arial, sans-serif; font-size: 16px;text-align: justify;'>
-	<p>Este é um exemplo de texto formatado em markdown. 
+	Este é um exemplo de texto formatado em markdown. 
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend, velit et malesuada euismod, risus massa convallis dui, ac bibendum libero eros non nisi. Aliquam erat volutpat. Mauris eros augue, eleifend suscipit quam non, faucibus lacinia erat. Nulla a mollis dolor. Vivamus ultricies, neque eget hendrerit congue, turpis sem condimentum libero, et pretium ipsum nibh quis ex. Curabitur sollicitudin, nisi ac ornare mollis, nibh mi ullamcorper urna, vel aliquet ipsum felis eu metus. Nam sollicitudin magna vitae est aliquet, feugiat lobortis nulla vehicula. Sed aliquet vulputate ex, et lobortis metus auctor in. Duis consequat mi eu ligula semper, eu fermentum augue luctus. Nullam sed finibus eros. Cras nec aliquam orci. Sed ante libero, tempus vel pretium vitae, rutrum et lacus. Donec vitae urna malesuada odio commodo molestie. Praesent varius elit urna, ac aliquam nulla tempus id. </p>
 	* **Links:** [Clique aqui para mais informações](https://www.example.com)
 
  
-	""")
+	""", unsafe_allow_html=True)
     #Gráfico 13
     fig13 = go.Figure()
     fig13.add_trace(go.Scatter(x=inf_mens['Mês'], y=inf_mens["Inflação Mensal - Slz (%)"], name="Inflação Mensal - São Luís (%)"))
