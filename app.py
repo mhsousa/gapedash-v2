0b9e8c38d1968b77b139b7c53f6f621ae086eb84
Mario Henrique <102007914+mhsousa@users.noreply.github.com>
2024-08-27 13:10:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -673,13 +673,11 @@ considerando toda a série temporal.</p>
 
 with tab3:
     st.markdown("""
+    <p style='font-family: Arial, sans-serif; font-size: 16px;text-align: justify;'>
 	Este é um exemplo de texto formatado em markdown. 
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend, velit et malesuada euismod, risus massa convallis dui, ac bibendum libero eros non nisi. Aliquam erat volutpat. Mauris eros augue, eleifend suscipit quam non, faucibus lacinia erat. Nulla a mollis dolor. Vivamus ultricies, neque eget hendrerit congue, turpis sem condimentum libero, et pretium ipsum nibh quis ex. Curabitur sollicitudin, nisi ac ornare mollis, nibh mi ullamcorper urna, vel aliquet ipsum felis eu metus. Nam sollicitudin magna vitae est aliquet, feugiat lobortis nulla vehicula. Sed aliquet vulputate ex, et lobortis metus auctor in. Duis consequat mi eu ligula semper, eu fermentum augue luctus. Nullam sed finibus eros. Cras nec aliquam orci. Sed ante libero, tempus vel pretium vitae, rutrum et lacus. Donec vitae urna malesuada odio commodo molestie. Praesent varius elit urna, ac aliquam nulla tempus id.,
-	* **Listas:**
-	  * Item 1
-	  * Item 2
-	* **Links:** [Clique aqui para mais informações](https://www.example.com)
-	""")
+	</p>
+	""", unsafE_allow_html=True)
 
     fig12 = px.line(rend_hab, x='Trimestre', y="Rendimento habitual médio real – MA (número índice, 1T2012=100)")
     fig12.update_yaxes(tickformat=".2f")
